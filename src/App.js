a628a33feac529f483f35880a134f547062d5ebc
jimzers <38634441+jimzers@users.noreply.github.com>
2019-10-26 20:00:40 -0700

    add pose queue and testing array

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import PredictFrame from './PredictFrame';
 import Pose from './Pose'
 import './App.css';
+import PoseQueue from "./PoseQueue";
 
 class App extends Component {
   render() {
+      // ultimately you have all the things in one whole array. then you can constantly cut off the array from the beginning
+      let chicken = [{'name': 'fart', 'current': false},{'name': 'fart', 'current': false}, {'name': 'fart', 'current': false}, {'name': 'fart', 'current': false}, {'name': 'fart', 'current': false}, {'name': 'fart', 'current': false}];
+      chicken = [{'name': 'fart', 'current': false},{'name': 'fart', 'current': false}];
     return (
       <div className="App">
           <div className="Title">Poser</div>
@@ -12,7 +16,8 @@ class App extends Component {
               <PredictFrame/>
           </div>
           <div className="pose-queue">
-              <Pose/>
+              {/*<Pose name={'fart'} current={true} />*/}
+              <PoseQueue pose_q={chicken} />
           </div>
       </div>
     );
