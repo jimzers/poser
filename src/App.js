cfefa3cd6f375951e2fba74fd6cfd55159f279f0
jimzers <38634441+jimzers@users.noreply.github.com>
2019-10-26 23:29:29 -0700

    linting App.js

--- src/App.js
+++ src/App.js
@@ -5,31 +5,31 @@ import './App.css';
 import PoseQueue from "./PoseQueue";
 
 class App extends Component {
-  render() {
-      // ultimately you have all the things in one whole array. then you can constantly cut off the array from the beginning
-      let chicken = [
-          {'name': 'arms_up', 'current': false},
-          {'name': 'fart', 'current': false},
-          {'name': 'slant_right', 'current': false},
-          {'name': 't_pose', 'current': false},
-          {'name': 'touchdown', 'current': false},
-          {'name': 'slant_left', 'current': false}
-          ];
-      // chicken = [{'name': 'fart', 'current': false},{'name': 'fart', 'current': false}];
-    return (
-      <div className="App">
-          <div className="Title">Poser</div>
-          <div className="webcam-frame">
-              <PredictFrame/>
-          </div>
-          <div className="pose-queue">
-              <div className={"space"}/>
-              {/*<Pose name={'fart'} current={true} />*/}
-              <PoseQueue pose_q={chicken} />
-          </div>
-      </div>
-    );
-  }
+    render() {
+        // ultimately you have all the things in one whole array. then you can constantly cut off the array from the beginning
+        let chicken = [
+            {'name': 'arms_up', 'current': false},
+            {'name': 'fart', 'current': false},
+            {'name': 'slant_right', 'current': false},
+            {'name': 't_pose', 'current': false},
+            {'name': 'touchdown', 'current': false},
+            {'name': 'slant_left', 'current': false}
+        ];
+        // chicken = [{'name': 'fart', 'current': false},{'name': 'fart', 'current': false}];
+        return (
+            <div className="App">
+                <div className="Title">Poser</div>
+                <div className="webcam-frame">
+                    <PredictFrame/>
+                </div>
+                <div className="pose-queue">
+                    <div className={"space"}/>
+                    {/*<Pose name={'fart'} current={true} />*/}
+                    <PoseQueue pose_q={chicken} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
