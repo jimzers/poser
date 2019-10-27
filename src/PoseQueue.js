e7dfa1c5de188aeb37bb69e14d2f956f50c20e8c
jimzers <38634441+jimzers@users.noreply.github.com>
2019-10-26 19:58:37 -0700

    init pose queue

---
+++ src/PoseQueue.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './PoseQueue.css';
+import Pose from "./Pose";
+
+class PoseQueue extends Component {
+    constructor(props) {
+        super(props);
+        this.queue = props.pose_q;
+    }
+
+    render() {
+        if(typeof this.queue[0] !== 'undefined') {
+            this.queue[0]['current'] = true;
+        }
+
+        // show next 5 poses in queue
+
+        // two things for each q object - name, current
+        let pose_q = this.queue.slice(0, 5).map(
+            (pose) => (<Pose name={pose['name']} current={pose['current']}/>)
+        );
+        return(
+            <div className={"pose-queue-container"}>
+                {pose_q}
+            </div>
+        )
+    }
+}
+
+export default PoseQueue;
